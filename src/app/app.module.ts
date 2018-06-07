77c68e621da0a6d8d7059da395e32ee34e2eb245
José Gomes <jose.gomes@egplusww.com>
2018-06-07 15:55:13 +0100

    ExportHomepageComponentFromSearchModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SearchModule } from './modules/search/search.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { SearchComponent } from './modules/search/search/search.component';
+import { HomePageComponent } from './modules/search/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SearchModule
+  ],
+  exports:[
+    SearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -4,11 +4,14 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { SearchRoutingModule } from './search-routing.module';
+
 import { HomePageComponent } from './home-page/home-page.component';
 import { SearchComponent } from './search/search.component';
 import { SearchListComponent } from './search-list/search-list.component';
 import { SearchListLeftFiltersComponent } from './search-list-left-filters/search-list-left-filters.component';
 import { SearchListRightFiltersComponent } from './search-list-right-filters/search-list-right-filters.component';
+
+import { SearchService } from './search.service';
 
 @NgModule({
   imports: [
@@ -17,6 +20,8 @@ import { SearchListRightFiltersComponent } from './search-list-right-filters/sea
     BrowserAnimationsModule,
     
   ],
-  declarations: [HomePageComponent, SearchComponent, SearchListComponent, SearchListLeftFiltersComponent, SearchListRightFiltersComponent]
+  exports: [HomePageComponent],
+  declarations: [HomePageComponent, SearchComponent, SearchListComponent, SearchListLeftFiltersComponent, SearchListRightFiltersComponent],
+  providers: [SearchService]
 })
 export class SearchModule { }
